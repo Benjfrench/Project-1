5dc4a8bc25d6ff3ca0ada80e7b897c96695446e9
Benjfrench <benfrench@live.com.au>
2024-05-26 09:48:20 +0800

    Project Skeleton

---
+++ projectScope.js
@@ -0,0 +1,10 @@
+// 1) New repo
+// 2) Multiple pages
+// 3) Navigation
+// 4) Interaction with external data src
+// 5) Techniques: Animations, graphs, icons, fonts, tamplating, bootstrap
+
+// App description: an app for fans of the first 10 seasons of the simpsons. 
+// -Links to each season with a write up of each. 
+// -Episode finder for searching specific episodes, with a filter function. Use API
+// -Quick search for specific episodes by name
